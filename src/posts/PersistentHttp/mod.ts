ac1c13faed8db48492bad5a62ce0bd80bfb77c70
Patrick Canfield <pscale01@gmail.com>
2023-02-09 17:26:48 -0800

    Add persistent HTTP post

---
+++ src/posts/PersistentHttp/mod.ts
@@ -0,0 +1,7 @@
+
+import { readTextFileFromModule } from "~/util.ts";
+
+const postContent = await readTextFileFromModule('./post.html', import.meta)
+export const Post = () => {
+  return postContent
+};
