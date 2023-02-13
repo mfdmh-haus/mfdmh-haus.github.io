b3ec0a4eb59dbf30c9dc9135c6649800f7ff07fb
Patrick Canfield <pscale01@gmail.com>
2023-02-13 14:50:46 -0800

    Update PersistentHttp post

--- src/posts/PersistentHttp/mod.ts
+++ src/posts/PersistentHttp/mod.ts
@@ -1,7 +1,15 @@
 
 import { readTextFileFromModule } from "~/util.ts";
+import { HeroHeader } from "~/components/HeroHeader/mod.ts";
+import { renderTemplate } from "~/util/template.ts";
 
-const postContent = await readTextFileFromModule('./post.html', import.meta)
+const template = await readTextFileFromModule('./post.html', import.meta)
 export const Post = () => {
-  return postContent
+  return renderTemplate(template, {
+    header: HeroHeader({
+      imageUrl: "/static/images/smelling_roses.png",
+      headerHtml: "<h1>Smelling the roses with persistent HTTP connections</h1>",
+      filter: "sepia(1) opacity(0.3)"
+    })
+  })
 };
