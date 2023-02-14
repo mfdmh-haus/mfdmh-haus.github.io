7d99ed95e202f50c9c08e4f60ed72e8a62756a80
Patrick Canfield <pscale01@gmail.com>
2023-02-13 17:41:13 -0800

    add image caption and meta to HeroHeader

--- src/components/HeroHeader/mod.ts
+++ src/components/HeroHeader/mod.ts
@@ -5,6 +5,8 @@ const template = await readTextFileFromModule("./template.html", import.meta);
 interface HeroHeaderProps {
   imageUrl: string,
   headerHtml: string,
+  meta?: string,
+  imageCaption?: string,
   filter?: string,
   height?: string,
 }
@@ -13,8 +15,10 @@ export function HeroHeader(props: HeroHeaderProps) {
   useStyleSheet("./style.css", import.meta)
   const data: TemplateData = {
     ...props,
+    imageCaption: props.imageCaption || "",
+    meta: props.meta || "",
     filter: props.filter || "var(--filter)",
-    height: props.height || "var(--height)"
+    height: props.height || "var(--height)",
   }
   return renderTemplate(template, data);
 }
--- src/posts/Deno/mod.ts
+++ src/posts/Deno/mod.ts
@@ -6,8 +6,15 @@ export const Post = () => {
   return renderTemplate(template, {
     header: HeroHeader({
       imageUrl: "/static/images/deno_news.png",
-      headerHtml: "<h1>Discovering Deno</h1>",
-      height: "18rem"
+      headerHtml: `
+        <h1>Discovering Deno</h1>
+        `,
+      meta: `
+        <p>last updated
+        <time datetime="2023-02-09">2023, February 9th</time>
+        `,
+      height: "18rem",
+      imageCaption: "Deno reading deno.news by <a href=\"#Duncan\">Duncan</a>. <a href=\"https://deno.land/artwork\">More fantastic Deno artwork</a>"
     })
   })
 };
