514b6dc8f0aea8ba842aabd185808c9a6d05cffb
Patrick Canfield <pscale01@gmail.com>
2023-02-13 16:20:34 -0800

    add height prop to HeroHeader

--- src/components/HeroHeader/mod.ts
+++ src/components/HeroHeader/mod.ts
@@ -6,13 +6,15 @@ interface HeroHeaderProps {
   imageUrl: string,
   headerHtml: string,
   filter?: string,
+  height?: string,
 }
 
 export function HeroHeader(props: HeroHeaderProps) {
   useStyleSheet("./style.css", import.meta)
   const data: TemplateData = {
     ...props,
-    filter: props.filter || "var(--filter)"
+    filter: props.filter || "var(--filter)",
+    height: props.height || "var(--height)"
   }
   return renderTemplate(template, data);
 }
